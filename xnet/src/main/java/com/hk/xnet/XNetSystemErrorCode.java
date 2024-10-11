8f718367096509b03f76d732036e569cec97fa9e
Gjb <894635762@qq.com>
2024-10-11 09:47:07 +0800

    完善系统级别错误的处理

--- xnet/src/main/java/com/hk/xnet/XNetSystemErrorCode.java
+++ xnet/src/main/java/com/hk/xnet/XNetSystemErrorCode.java
@@ -5,18 +5,11 @@ package com.hk.xnet;
   系统级别Error
  */
 public enum XNetSystemErrorCode {
-
     EXIT_APP_ERROR("-999", "退出app"),
-
     START_OTHER_APP("-888","跳转到其他页面"),
-
     TOKEN_ERROR("10001", "token 失效"),
-
     DEVICE_ERROR("30011", "设备被挤出"),
-    DEVICE_ERROR_TEST("0", "测试被挤出！！！"),
-    //  public final static String ERROR_NET_WORK = "-444";
     NET_ERROR("-444","网络异常");
-
     private String mCode;
     private String mMsg;
     XNetSystemErrorCode(String code, String message) {
@@ -26,6 +19,10 @@ public enum XNetSystemErrorCode {
 
     public String getCode() {
         return mCode;
+    }
+
+    public String getMsg() {
+        return mMsg;
     }
 
     public boolean isSystemErrorCode(String code) {
